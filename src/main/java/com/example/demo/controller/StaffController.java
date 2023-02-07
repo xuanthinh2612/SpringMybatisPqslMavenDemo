83824a54bf0e85e04712efb73596436c37668ad2
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-02-07 18:16:39 +0700

    crud

--- src/main/java/com/example/demo/controller/StaffController.java
+++ src/main/java/com/example/demo/controller/StaffController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.Staff;
